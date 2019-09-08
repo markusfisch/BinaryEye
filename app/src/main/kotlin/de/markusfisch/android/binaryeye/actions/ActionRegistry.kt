d499b78c06bebfa42b9f0212c693d82c56668b92
Molikuner <molikuner@gmail.com>
2019-09-08 19:10:55 +0200

    Add OTP support
    * Added TestUtils

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/actions/ActionRegistry.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/actions/ActionRegistry.kt
@@ -1,6 +1,7 @@
 package de.markusfisch.android.binaryeye.actions
 
 import de.markusfisch.android.binaryeye.actions.mail.MailAction
+import de.markusfisch.android.binaryeye.actions.otpauth.OtpauthAction
 import de.markusfisch.android.binaryeye.actions.sms.SmsAction
 import de.markusfisch.android.binaryeye.actions.tel.TelAction
 import de.markusfisch.android.binaryeye.actions.vtype.vcard.VCardAction
@@ -9,7 +10,7 @@ import de.markusfisch.android.binaryeye.actions.wifi.WifiAction
 
 object ActionRegistry {
 	val REGISTRY: Set<IAction> = setOf(
-		MailAction, SmsAction, TelAction, VCardAction, VEventAction, WifiAction
+		MailAction, OtpauthAction, SmsAction, TelAction, VCardAction, VEventAction, WifiAction
 	)
 
 	fun getAction(data: ByteArray): IAction? = REGISTRY.find {
