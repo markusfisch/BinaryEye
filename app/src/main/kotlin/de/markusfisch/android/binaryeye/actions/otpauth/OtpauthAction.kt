977b05ef91c66691bb2169a892c8751e54dd0dbd
Molikuner <molikuner@gmail.com>
2019-09-29 21:55:11 +0200

    Remove parsing of OTPs

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/actions/otpauth/OtpauthAction.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/actions/otpauth/OtpauthAction.kt
@@ -1,22 +1,10 @@
 package de.markusfisch.android.binaryeye.actions.otpauth
 
-import android.content.Context
-import android.content.Intent
 import de.markusfisch.android.binaryeye.R
-import de.markusfisch.android.binaryeye.actions.IntentAction
+import de.markusfisch.android.binaryeye.actions.SchemeAction
 
-object OtpauthAction : IntentAction() {
+object OtpauthAction : SchemeAction() {
 	override val iconResId: Int = R.drawable.ic_action_otpauth
 	override val titleResId: Int = R.string.otpauth_add
-	override val errorMsg: Int = R.string.otpauth_error
-
-	override fun canExecuteOn(data: ByteArray): Boolean {
-		return OtpauthParser(String(data)) != null
-	}
-
-	override suspend fun createIntent(context: Context, data: ByteArray): Intent? {
-		return OtpauthParser(String(data))?.let {
-			Intent(Intent.ACTION_VIEW, it.uri)
-		}
-	}
+	override val scheme: String = "otpauth"
 }
