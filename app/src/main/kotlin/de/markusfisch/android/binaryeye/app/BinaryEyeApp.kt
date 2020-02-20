5d56365e79e02d3909284a9a42ffb667102191ae
Markus Fisch <mf@markusfisch.de>
2020-02-20 20:08:52 +0100

    Skip forceCompat() when building on Catalina
    Calling RenderScript.forceCompat() on a build that was made with the
    new build tools (which are required to make RenderScript build again
    on macOS Catalina), results in a RSRuntimeException (Loading of ScriptC
    script failed).
    
    RenderScript.forceCompat() was introduced to fix RenderScript for
    Lineage 16.0. Maybe this is no longer required with those new build
    tools?
    
    Yes, this means this setup is producing platform independent builds
    that have different features depending on what platform they were
    built on. And yes, that's as bad as it sounds.
    
    So, for the time being, it's probably better to _NOT_ use Catalina
    to make release builds.

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/app/BinaryEyeApp.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/app/BinaryEyeApp.kt
@@ -2,6 +2,7 @@ package de.markusfisch.android.binaryeye.app
 
 import android.app.Application
 import android.support.v8.renderscript.RenderScript
+import de.markusfisch.android.binaryeye.BuildConfig
 import de.markusfisch.android.binaryeye.preference.Preferences
 import de.markusfisch.android.binaryeye.repository.DatabaseRepository
 
@@ -12,9 +13,19 @@ class BinaryEyeApp : Application() {
 	override fun onCreate() {
 		super.onCreate()
 
-		// required to make RenderScript work for Lineage 16.0
-		// possibly because of a system/device bug
-		RenderScript.forceCompat()
+		// Since RenderScript can no longer be compiled on macOS Catalina
+		// because Catalina cannot run 32bit binaries and Google failed
+		// to provide a 64bit compiler in time. Now, as if that wasn't
+		// bad enough, `RenderScript.forceCompat()` makes any build compiled
+		// with the new build tools that provide a 64bit compiler crash
+		// so we need to exclude this if we're building on Catalina.
+		// Maybe `RenderScript.forceCompat()` isn't required anymore for
+		// those new builds but I don't have a device to test this.
+		if (!BuildConfig.IS_CATALINA) {
+			// required to make RenderScript work for Lineage 16.0
+			// possibly because of a system/device bug
+			RenderScript.forceCompat()
+		}
 
 		db.open(this)
 		prefs.init(this)
