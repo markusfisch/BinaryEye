b929df48489eb2ab2780a4e293c51f2da3d01e6f
Markus Fisch <mf@markusfisch.de>
2020-03-14 15:56:53 +0100

    Always set window inset listener
    In case there is a configuration (orientation) change later on.
    
    And always return previously initialized window insets what is
    required when Fragments change and the listener won't fire (again).
    
    Also use an explicit variable to know when the window insets rect
    has been initialized. `Rect.isEmpty()` doesn't cut the mustard.

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/app/SystemBars.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/app/SystemBars.kt
@@ -81,17 +81,22 @@ private fun getAppCompatActivity(context: Context): AppCompatActivity? {
 	return null
 }
 
+private val windowInsets = Rect()
+private var windowInsetsSet = false
 private var windowInsetsListener: ((insets: Rect) -> Unit)? = null
+
 // invoke this from Fragment.onCreateView() *or* Activity.onCreate()
 fun setWindowInsetListener(listener: (insets: Rect) -> Unit) {
-	if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+	if (windowInsetsSet) {
+		// invoke listener for previously set window insets because
+		// setOnApplyWindowInsetsListener() won't fire again if we
+		// change Fragments
 		listener(windowInsets)
-	} else {
-		windowInsetsListener = listener
 	}
+	// *always* set the listener in case there is a configuration change
+	windowInsetsListener = listener
 }
 
-private val windowInsets = Rect()
 // invoke this from Activity.onCreate() to setup listening for insets
 fun setupInsets(view: View, toolbar: Toolbar) {
 	val toolBarHeight = toolbar.layoutParams.height
@@ -99,6 +104,7 @@ fun setupInsets(view: View, toolbar: Toolbar) {
 		// save insets so setWindowInsetListener() can return them
 		// when it's called from Fragment.onCreateView() later on
 		windowInsets.set(0, toolBarHeight, 0, 0)
+		windowInsetsSet = true
 		windowInsetsListener?.also { it(windowInsets) }
 		return
 	}
@@ -111,6 +117,7 @@ fun setupInsets(view: View, toolbar: Toolbar) {
 				insets.systemWindowInsetRight,
 				insets.systemWindowInsetBottom
 			)
+			windowInsetsSet = true
 			windowInsetsListener?.also { it(windowInsets) }
 		}
 		insets
