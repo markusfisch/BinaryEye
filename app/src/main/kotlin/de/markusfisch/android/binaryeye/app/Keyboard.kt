41e538fa97998b902900fa705102980bc797c845
Markus Fisch <mf@markusfisch.de>
2020-04-13 17:56:09 +0200

    Remove unused function to show soft keyboard

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/app/Keyboard.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/app/Keyboard.kt
@@ -4,14 +4,6 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
-fun Context.showSoftKeyboard() {
-	(getSystemService(
-		Context.INPUT_METHOD_SERVICE
-	) as InputMethodManager).toggleSoftInput(
-		InputMethodManager.SHOW_FORCED, 0
-	)
-}
-
 fun Context.hideSoftKeyboard(view: View) {
 	(getSystemService(
 		Context.INPUT_METHOD_SERVICE
