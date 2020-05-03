ea0212c58eb9d50726a2db887c15352c77b5a0b0
Markus Fisch <mf@markusfisch.de>
2020-05-03 19:03:55 +0200

    Remove special treatment for macOS Catalina
    Since 64bit Linux systems can build with build tools 29.0.3 too
    and RenderScript.forceCompat() is only executed for Lineage OS
    now.
    
    Still, do NOT build release versions with macOS Catalina or the
    build will break on Lineage OS!

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/app/BinaryEyeApp.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/app/BinaryEyeApp.kt
@@ -2,7 +2,6 @@ package de.markusfisch.android.binaryeye.app
 
 import android.app.Application
 import android.support.v8.renderscript.RenderScript
-import de.markusfisch.android.binaryeye.BuildConfig
 import de.markusfisch.android.binaryeye.data.Database
 import de.markusfisch.android.binaryeye.preference.Preferences
 
@@ -13,18 +12,11 @@ class BinaryEyeApp : Application() {
 	override fun onCreate() {
 		super.onCreate()
 
-		// `RenderScript.forceCompat()` will crash the app when compiled
-		// on macOS Catalina with build tools 29.0.3 :( To make things
-		// even more complicated, `forceCompat()` will continue to work
-		// if the app is compiled on Linux with the exact same version
-		// 29.0.3 of the build tools
-		if (!BuildConfig.IS_CATALINA &&
-			System.getProperty("os.version")?.contains(
+		if (System.getProperty("os.version")?.contains(
 				"lineageos", true
 			) == true
 		) {
-			// required to make RenderScript work for Lineage 16.0
-			// possibly because of a system/device bug
+			// required to make RenderScript work on Lineage 16.0
 			RenderScript.forceCompat()
 		}
 
