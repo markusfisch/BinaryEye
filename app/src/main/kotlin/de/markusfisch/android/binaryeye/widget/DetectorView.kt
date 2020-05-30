9b1445efb5ba0ff4e143ac499cc5f5b7be979304
Markus Fisch <mf@markusfisch.de>
2020-05-30 14:32:35 +0200

    Draw a dotted rectangle around ROI below Kitkat
    Because Canvas.clipRect() doesn't work very well below
    Kitkat.

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/widget/DetectorView.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/widget/DetectorView.kt
@@ -26,18 +26,30 @@ class DetectorView : View {
 		marks = null
 		invalidate()
 	}
+	private val roiPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 	private val handleBitmap = resources.getBitmapFromDrawable(
 		R.drawable.ic_crop_handle
 	)
 	private val handleXRadius = handleBitmap.width / 2
 	private val handleYRadius = handleBitmap.height / 2
-	private val distToFull = 24f * context.resources.displayMetrics.density
+	private val distToFull: Float
 
 	private var marks: List<Point>? = null
 	private var center = PointF()
 	private var handlePos = PointF()
 	private var handleGrabbed = false
 	private var shadeColor = 0
+
+	init {
+		val dp = context.resources.displayMetrics.density
+		distToFull = 24f * dp
+		roiPaint.apply {
+			style = Paint.Style.STROKE
+			color = 0xffffffff.toInt()
+			strokeWidth = 2f * dp
+			pathEffect = DashPathEffect(floatArrayOf(10f, 20f), 0f)
+		}
+	}
 
 	constructor(context: Context, attrs: AttributeSet) :
 			super(context, attrs)
@@ -123,10 +135,15 @@ class DetectorView : View {
 		canvas.drawColor(0, PorterDuff.Mode.CLEAR)
 		updateClipRect()
 		if (roi.height() > 0 && roi.width() > 0) {
-			canvas.save()
-			canvas.clipOutRectCompat(roi)
-			canvas.drawColor(shadeColor, PorterDuff.Mode.SRC)
-			canvas.restore()
+			// canvas.clipRect() doesn't work reliably below KITKAT
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+				canvas.save()
+				canvas.clipOutRectCompat(roi)
+				canvas.drawColor(shadeColor, PorterDuff.Mode.SRC)
+				canvas.restore()
+			} else {
+				canvas.drawRect(roi, roiPaint)
+			}
 		}
 		marks?.let {
 			candidates.draw(canvas, it)
