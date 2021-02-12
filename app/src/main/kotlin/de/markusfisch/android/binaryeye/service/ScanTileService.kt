e6d9d548660968477dcfe0ea10630de5119d6278
Markus Fisch <mf@markusfisch.de>
2021-02-12 21:48:59 +0100

    Add a quick settings tile to scan from settings

---
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/service/ScanTileService.kt
@@ -0,0 +1,18 @@
+package de.markusfisch.android.binaryeye.service
+
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
+import android.support.annotation.RequiresApi
+import de.markusfisch.android.binaryeye.activity.CameraActivity
+
+@RequiresApi(Build.VERSION_CODES.N)
+class ScanTileService : TileService() {
+	@RequiresApi(Build.VERSION_CODES.N)
+	override fun onClick() {
+		super.onClick()
+		val intent = Intent(applicationContext, CameraActivity::class.java)
+		intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+		startActivityAndCollapse(intent)
+	}
+}
