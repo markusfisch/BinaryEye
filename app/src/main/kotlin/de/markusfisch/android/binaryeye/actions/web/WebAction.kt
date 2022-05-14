6852b6523960762c9ebb283759cad93f5b97eb10
Markus Fisch <mf@markusfisch.de>
2022-05-14 12:50:22 +0200

    Recognize URLs in colloquial, incomplete form too
    So "google.com" is recognized as web action.

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/actions/web/WebAction.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/actions/web/WebAction.kt
@@ -1,11 +1,27 @@
 package de.markusfisch.android.binaryeye.actions.web
 
+import android.content.Context
 import de.markusfisch.android.binaryeye.R
-import de.markusfisch.android.binaryeye.actions.SchemeAction
+import de.markusfisch.android.binaryeye.actions.IAction
+import de.markusfisch.android.binaryeye.content.openUrl
 
-object WebAction : SchemeAction() {
+object WebAction : IAction {
+	private val colloquialRegex = """^(http[s]*://)*([a-z0-9]+[a-z0-9-]*[a-z0-9]+[.])+[a-z]{2,}([?#/]+[\w/=&;._-]*)*$""".toRegex(
+		RegexOption.IGNORE_CASE
+	)
+
 	override val iconResId: Int = R.drawable.ic_action_open
 	override val titleResId: Int = R.string.open_url
-	override val scheme: String = "https?"
-	override val buildRegex: Boolean = true
+
+	override fun canExecuteOn(data: ByteArray): Boolean {
+		return String(data).trim().matches(colloquialRegex)
+	}
+
+	override suspend fun execute(context: Context, data: ByteArray) {
+		var url = String(data).trim()
+		if (!url.startsWith("http") && !url.startsWith("ftp")) {
+			url = "http://${url}"
+		}
+		context.openUrl(url)
+	}
 }
