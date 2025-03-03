751d8932f8e874c2b8c8de9284cce688391c005a
Markus Fisch <mf@markusfisch.de>
2025-03-03 23:47:16 +0100

    Immediately replace toasts
    Instead of showing them in sequence.
    
    Because in this app, only the last message is important.
    
    Speeds up continuous scanning. Especially when the scanned code is
    also forwarded and the server result is displayed in the next toast
    (see #523).

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/widget/Toast.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/widget/Toast.kt
@@ -3,19 +3,29 @@ package de.markusfisch.android.binaryeye.widget
 import android.content.Context
 import android.widget.Toast
 
-fun Context.toast(message: Int) = Toast.makeText(
-	applicationContext,
-	message,
-	Toast.LENGTH_LONG
-).show()
+private var lastToast: Toast? = null
 
-fun Context.toast(message: String) = Toast.makeText(
-	applicationContext,
-	message.ellipsize(128),
-	Toast.LENGTH_LONG
-).show()
+fun <T> Context.toast(message: T) {
+	lastToast?.cancel()
+	lastToast = when (message) {
+		is String -> Toast.makeText(
+			applicationContext,
+			message.ellipsize(),
+			Toast.LENGTH_LONG
+		)
 
-private fun String.ellipsize(max: Int) = if (length < max) {
+		is Int -> Toast.makeText(
+			applicationContext,
+			message,
+			Toast.LENGTH_LONG
+		)
+
+		else -> return
+	}
+	lastToast?.show()
+}
+
+private fun String.ellipsize(max: Int = 128) = if (length < max) {
 	this
 } else {
 	"${take(max)}…"
