4b2eb80612c01a962f44dbfbc767ce3acedaf871
Markus Fisch <mf@markusfisch.de>
2026-02-24 23:52:04 +0100

    Fix margin for 1D barcodes
    Since ZXingCpp v3.0.0+ changed the default from -1 to 0.
    
    Using -1 results in an exception in `BitMatrix::setRegion()`
    because of invalid top/left coordinates.

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/fragment/EncodeFragment.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/fragment/EncodeFragment.kt
@@ -273,7 +273,7 @@ class EncodeFragment : Fragment() {
 					addQuietZoneSwitch.isChecked
 				) 1 else 0
 
-				else -> -1
+				else -> 0
 			},
 			if (format.canBeInverted()) {
 				colorsSpinner.selectedItemPosition
