9dccc6a7f524f4e144dc2d171ca27e81ea752297
Markus Fisch <mf@markusfisch.de>
2026-04-09 20:32:11 +0200

    Fix EPC QR parser tests

--- app/src/test/kotlin/de/markusfisch/android/binaryeye/content/EpcQrParserTest.kt
+++ app/src/test/kotlin/de/markusfisch/android/binaryeye/content/EpcQrParserTest.kt
@@ -11,12 +11,12 @@ class EpcQrParserTest {
 			"BCD\n002\n1\nSCT\n\nWikimedia Foerdergesellschaft\nDE12345678901234567890"
 		) ?: throw AssertionError("expected valid EPC QR content")
 
-		assertEquals("BCD", info.elements[EpcQrElement.SERVICE_TAG])
-		assertEquals("002", info.elements[EpcQrElement.VERSION])
-		assertEquals("UTF-8", info.elements[EpcQrElement.CHARSET])
-		assertEquals("SCT", info.elements[EpcQrElement.IDENTIFICATION])
-		assertEquals("Wikimedia Foerdergesellschaft", info.elements[EpcQrElement.NAME])
-		assertEquals("DE12345678901234567890", info.elements[EpcQrElement.IBAN])
+		assertEquals("BCD", info[EpcQrElement.SERVICE_TAG])
+		assertEquals("002", info[EpcQrElement.VERSION])
+		assertEquals("UTF-8", info[EpcQrElement.CHARSET])
+		assertEquals("SCT", info[EpcQrElement.IDENTIFICATION])
+		assertEquals("Wikimedia Foerdergesellschaft", info[EpcQrElement.NAME])
+		assertEquals("DE12345678901234567890", info[EpcQrElement.IBAN])
 	}
 
 	@Test
@@ -25,11 +25,11 @@ class EpcQrParserTest {
 			"BCD\r\n001\r\n1\r\nSCT\r\nBFSWDE33BER\r\nWikimedia Foerdergesellschaft\r\nDE12345678901234567890\r\nEUR123.45\r\nBENE\r\nRF18539007547034\r\n\r\nInvoice 2026"
 		) ?: throw AssertionError("expected valid EPC QR content")
 
-		assertEquals("BFSWDE33BER", info.elements[EpcQrElement.BIC])
-		assertEquals("123.45", info.elements[EpcQrElement.AMOUNT])
-		assertEquals("BENE", info.elements[EpcQrElement.PURPOSE])
-		assertEquals("RF18539007547034", info.elements[EpcQrElement.REFERENCE])
-		assertEquals("Invoice 2026", info.elements[EpcQrElement.INFORMATION])
+		assertEquals("BFSWDE33BER", info[EpcQrElement.BIC])
+		assertEquals("123.45", info[EpcQrElement.AMOUNT])
+		assertEquals("BENE", info[EpcQrElement.PURPOSE])
+		assertEquals("RF18539007547034", info[EpcQrElement.REFERENCE])
+		assertEquals("Invoice 2026", info[EpcQrElement.INFORMATION])
 	}
 
 	@Test
